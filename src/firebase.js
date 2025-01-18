bb8b420094e86ede24fe8eacb2e31d7faada31d1
suleman098 <81332154+suleman098@users.noreply.github.com>
2025-01-18 23:37:39 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyBkYeEX1JmwdPn3OPhwnGK_R_wAMBJnDoo",
-  authDomain: "jobsearch-34ebe.firebaseapp.com",
-  projectId: "jobsearch-34ebe",
-  storageBucket: "jobsearch-34ebe.appspot.com",
-  messagingSenderId: "426913957504",
-  appId: "1:426913957504:web:3ca24aebc8dff9525b8865",
-  measurementId: "G-W1KYTNWW7L",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
